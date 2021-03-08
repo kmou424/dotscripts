cbb7a56e4ebeaad88adf4a509c0103ce3f154508
kmou424 <dx2689282850@gmail.com>
2021-03-08 14:47:30 +0800

    kernel: Do not build dtbo.img because source will build by default
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -79,7 +79,7 @@ DEF_REG=0
 
 # Build dtbo.img (select this only if your source has support to building dtbo.img)
 # 1 is YES | 0 is NO(default)
-BUILD_DTBO=1
+BUILD_DTBO=0
 	if [ $BUILD_DTBO = 1 ]
 	then 
 		# Set this to your dtbo path. 
