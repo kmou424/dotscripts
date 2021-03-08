d01bd691a713988ccecaed4d2e502dacf4242e4b
kmou424 <dx2689282850@gmail.com>
2021-03-08 14:46:08 +0800

    Revert "kernel: Fix build dtbo with common way"
    This reverts commit e918347bedaad9bd0328af41165505758941770f.

--- kernel.sh
+++ kernel.sh
@@ -80,6 +80,13 @@ DEF_REG=0
 # Build dtbo.img (select this only if your source has support to building dtbo.img)
 # 1 is YES | 0 is NO(default)
 BUILD_DTBO=1
+	if [ $BUILD_DTBO = 1 ]
+	then 
+		# Set this to your dtbo path. 
+		# Defaults in folder out/arch/arm64/boot/dts
+		DTBO_PATH="xiaomi/raphael-sm8150-overlay.dtbo"
+	fi
+
 # Sign the zipfile
 # 1 is YES | 0 is NO
 SIGN=1
@@ -292,7 +299,7 @@ build_kernel() {
 				msg "|| Building DTBO ||"
 				tg_post_msg "<code>Building DTBO..</code>"
 				python2 "$KERNEL_DIR/scripts/ufdt/libufdt/utils/src/mkdtboimg.py" \
-					create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" "$KERNEL_DIR/out/arch/arm64/boot/dts/qcom/*.dtbo"
+					create "$KERNEL_DIR/out/arch/arm64/boot/dtbo.img" --page_size=4096 "$KERNEL_DIR/out/arch/arm64/boot/dts/$DTBO_PATH"
 			fi
 				gen_zip
 			else
