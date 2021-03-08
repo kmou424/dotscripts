2044a5a2af91e0272dcef0cfe35de3d353b954ce
kmou424 <dx2689282850@gmail.com>
2021-03-08 23:42:50 +0800

    kernel: Migrate chat
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -80,7 +80,7 @@ PTTG=1
 	if [ $PTTG = 1 ]
 	then
 		# Set Telegram Chat ID
-		CHATID="-559760794"
+		CHATID="-496638408"
 	fi
 
 # Generate a full DEFCONFIG prior building. 1 is YES | 0 is NO(default)
