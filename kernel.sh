ece3ac98b60234c272117f9ffed7fe220361163d
kmou424 <dx2689282850@gmail.com>
2021-03-09 10:04:08 +0800

    kernel: Add DTBO_AVAILABLE param to tg msg
    Signed-off-by: kmou424 <dx2689282850@gmail.com>

--- kernel.sh
+++ kernel.sh
@@ -98,6 +98,13 @@ BUILD_DTBO=0
 
 EXTERNAL_DTBO=1
 
+DTBO_AVAILABLE="否"
+
+	if [ $BUILD_DTBO = 1 ] || [ $EXTERNAL_DTBO = 1 ]
+	then
+		DTBO_AVAILABLE="是"
+	fi
+
 # Sign the zipfile
 # 1 is YES | 0 is NO
 SIGN=0
@@ -273,7 +280,7 @@ build_kernel() {
 
 	if [ "$PTTG" = 1 ]
  	then
-		tg_post_msg "<b>第 $KBUILD_BUILD_VERSION CI构建任务开始</b>%0A<b>操作系统 : </b><code>$DISTRO</code>%0A<b>$ZIPNAME 版本 : </b><code>$KERNEL_VERSION</code>%0A<b>Linux 版本 : </b><code>$KERVER</code>%0A<b>日期 : </b><code>$(export TZ=UTC-8; date)</code>%0A<b>设备 : </b><code>$MODEL [$DEVICE]</code>%0A<b>CI 服务商 : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>核心数 : </b><code>$PROCS</code>%0A<b>工具链 : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Linker : </b><code>$LINKER</code>%0a<b>构建分支 : </b><code>$CI_BRANCH</code>%0A<b>内核 HEAD Commit : </b><code>$COMMIT_HEAD</code>%0A<b>脚本 HEAD Commit : </b><code>$SCRIPT_COMMIT_HEAD</code>%0A<b>构建类型 : </b>#Nightly"
+		tg_post_msg "<b>第 $KBUILD_BUILD_VERSION CI构建任务开始</b>%0A<b>操作系统 : </b><code>$DISTRO</code>%0A<b>$ZIPNAME 版本 : </b><code>$KERNEL_VERSION</code>%0A<b>Linux 版本 : </b><code>$KERVER</code>%0A<b>日期 : </b><code>$(export TZ=UTC-8; date)</code>%0A<b>设备 : </b><code>$MODEL [$DEVICE]</code>%0A<b>CI 服务商 : </b><code>$KBUILD_BUILD_HOST</code>%0A<b>核心数 : </b><code>$PROCS</code>%0A<b>工具链 : </b><code>$KBUILD_COMPILER_STRING</code>%0A<b>Linker : </b><code>$LINKER</code>%0A<b>构建分支 : </b><code>$CI_BRANCH</code>%0A<b>包含dtbo的构建 : </b><code>$DTBO_AVAILABLE</code>%0A<b>内核 HEAD Commit : </b><code>$COMMIT_HEAD</code>%0A<b>脚本 HEAD Commit : </b><code>$SCRIPT_COMMIT_HEAD</code>%0A<b>构建类型 : </b>#Nightly"
 	fi
 
 	make O=out $DEFCONFIG
